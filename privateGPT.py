ee10e545fbfc924817051b5d0320cffb673e4a48
zach <4543950+zma00@users.noreply.github.com>
2023-08-07 11:15:52 -0400

    performance tweaks

--- privateGPT.py
+++ privateGPT.py
@@ -33,7 +33,7 @@ def main():
     # Prepare the LLM
     match model_type:
         case "LlamaCpp":
-            llm = LlamaCpp(model_path=model_path, max_tokens=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks, verbose=False)
+            llm = LlamaCpp(model_path=model_path, max_tokens=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks, n_gpu_layers=43, verbose=False)
         case "GPT4All":
             llm = GPT4All(model=model_path, max_tokens=model_n_ctx, backend='gptj', n_batch=model_n_batch, callbacks=callbacks, verbose=False)
         case _default:
@@ -69,7 +69,7 @@ def main():
 def parse_arguments():
     parser = argparse.ArgumentParser(description='privateGPT: Ask questions to your documents without an internet connection, '
                                                  'using the power of LLMs.')
-    parser.add_argument("--hide-source", "-S", action='store_true',
+    parser.add_argument("--hide-source", "-S", action='store_true', default=True,
                         help='Use this flag to disable printing of source documents used for answers.')
 
     parser.add_argument("--mute-stream", "-M",
